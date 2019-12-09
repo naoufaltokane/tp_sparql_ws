8699b5f28fa888664941d456f23624348d249d47
Naoufal Tokane <naoufaltokane@gmail.com>
2019-12-09 16:42:51 +0100

    update

--- src/tp_sparql_ws/Main.java
+++ src/tp_sparql_ws/Main.java
@@ -26,8 +26,37 @@ public class Main {
 		String queryString =
 			"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "+
 			"PREFIX vCard: <http://www.w3.org/2001/vcard-rdf/3.0#> " +
-				//2
-			"SELECT  * WHERE {?subject ?predicate ?object }LIMIT 3";
+			//1
+			 "SELECT  * WHERE {?subject ?predicate ?object }";
+			 
+			//2
+			 //"SELECT  * WHERE {?subject ?predicate ?object }LIMIT 3";
+			
+			//3
+			//"SELECT  ?subject ?predicate ?object WHERE {?subject ?predicate ?object FILTER (?object <= '2019' )} ORDER BY asc(?object)";
+	    
+			//4
+			//"SELECT ?object WHERE {?subject ?predicate ?object FILTER (?object <= '2018' )}";
+			 
+			//2.1
+			//"SELECT  (count( ?object) as ?total) WHERE {?subject vCard:Promotion ?object FILTER (?object <= '2020' ) }";
+				
+			//2.2
+			//"SELECT  (max( ?object) as ?Derniere_annee) WHERE {?subject vCard:Promotion ?object }";
+			//"SELECt ?object Where {?subject vCard:Promotion ?object } Order by desc(?object) LIMIT 1"		;
+
+			//2.3
+			//"SELECT ?faculte ?promotion ?master WHERE { ?s vCard:MASTER  ?master. ?s  vCard:Promotion ?promotion. ?s vCard:Faculte ?faculte }"
+			//+ "ORDER BY desc(?promotion)"
+			//+ "LIMIT 1";
+
+			//2.4.1
+			//"SELECT  (min( ?object) as ?premiere_annee) WHERE {?subject vCard:Promotion ?object }";
+						
+			//2.4.2
+			//"SELECT ?faculte ?promotion ?master WHERE { ?s vCard:MASTER  ?master. ?s  vCard:Promotion ?promotion. ?s vCard:Faculte ?faculte }"
+			//+ "ORDER BY asc(?promotion)"
+			//+ "LIMIT 1";
 		
 		Query query = QueryFactory.create(queryString);
 		QueryExecution ms = QueryExecutionFactory.create(query, model);
